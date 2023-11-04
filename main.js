51d704d47c32d02a41ddd4973c9cd02b5d06bfdc
otmak <jakelovz@gmail.com>
2023-11-04 16:52:57 -0700

    ++ buttons and some style

--- main.js
+++ main.js
@@ -2,63 +2,96 @@
 
 mainCanvas.width = 500;
 mainCanvas.height = 400;
+const selectedBtn = document.getElementById('algorithms');
 
-const num = 30;
+const num = 14;
 const arr = [];
 const obj = {};
 const margin = 30;
 
-const cols = [];
+let cols = [];
 const spacing = (mainCanvas.width - margin*2)/num;
 const ctx = mainCanvas.getContext('2d');
+let operations = [];
+let theChosenOne = '';
 
 
 // Create array of random numbers between 0 and 1.
-for(let i=0; i<num; i++){
-	const randomNum =  Math.random()
-	arr[i] = randomNum;
-	obj[randomNum] = i;
+initiateArray();
 
+function initiateArray(){
+
+	for(let i=0; i<num; i++){
+		const randomNum =  Math.random()
+		arr[i] = randomNum;
+		obj[randomNum] = i;
+	}
+	paint();
+	requestAnimationFrame(animate);
+};
+
+function paint(){
+	operations = [];
+	for(let i=0; i<arr.length; i++){
+		const x = i*spacing+spacing/2 + margin;
+		const y = mainCanvas.height - margin;
+		const width = spacing -4;
+		const height = (mainCanvas.height - margin*2)*arr[i];
+		cols[i] = new Column(x,y,width,height);// Create colums
+	}
+}
+
+
+const selectAgorithm = (arg, i) =>{
+	const dup = [...arg];
+	const object = {
+		a: bubbleSort(dup),
+		b: insertionSort(dup),
+		c: mergeSort(dup),
+	}
+		// 	c: mergeSort(arg),
+		// d: heapSort(arg),
+		// e: quickSort(arg)
+	return object[i];
+}
+
+
+selectedBtn.addEventListener('click', (e)=>{
+	if(e.target.nodeName === 'SPAN'){
+		paint();
+		theChosenOne = e.target.id;
+	}
+})
+
+
+// Run the Algorithm.
+function begin(){
+	operations = [];
+	operations = selectAgorithm(arr, theChosenOne)
+	requestAnimationFrame(animate);
+	// animate();
 }
 
 // TEST copy array.
-const test_Array = [...arr];
-console.log(arr)
-
+// const test_Array = [...arr];
 //linear interpolation
 function lInterpolate(a, b, t){
 	return a + (b - a) * t;
 }
 
-// console.log(arr)
-// console.log(ctx, spacing )
-
-for(let i=0; i<arr.length; i++){
-
-	const x = i*spacing+spacing/2 + margin;
-	const y = mainCanvas.height - margin;
-	const width = spacing -4;
-	const height = (mainCanvas.height - margin*2)*arr[i];
-	cols[i] = new Column(x,y,width,height);// Create colums
-	// cols[i].draw(ctx);
-	// console.log(cols[i])
-}
-
-// let operations = bubbleSort_2(arr);
-// let operations = insertionSort(arr);
-let operations = mergeSort(arr);
-
 
 animate();
-
 function animate(){
+	let counter = 0;
 
 	ctx.clearRect(0,0, mainCanvas.width, mainCanvas.height);
 	let processing = false;
 	for( let i=0; i<cols.length; i++ ){
 		processing = cols[i].draw(ctx) || processing;
+		counter++
 	}
-	console.log(operations)
+
+	// if undifined or something give an alert;
 	if( !processing && operations.length >0 ){
 		const action = operations.shift();
 		const [i,j] = action.index;
@@ -68,93 +101,114 @@ function animate(){
 			[ cols[i], cols[j] ] = [ cols[j], cols[i] ]; //Swap
 		}
 	}
-	requestAnimationFrame(animate);
+	if(operations.length > 0){
+		requestAnimationFrame(animate);
+	}
 }
+
 
 /*********** Sorting algorithms *************/
 
 
 // BUBBLE SORT.
-function bubbleSort_2(arr){
-	const operations = [];
+function bubbleSort(arg){
+	const array = [...arg];
+	const actions = [];
 	// let isSwapped = false;
 	do{
 		var isSwapped = false;
-		for(let i=0; i<arr.length; i++){
-			let current = arr[i];
-			if(arr[i] > arr[i+1]){
+		for(let i=0; i<array.length; i++){
+			let current = array[i];
+			if(array[i] > array[i+1]){
 				isSwapped = true;
-				[ arr[i], arr[i+1]] = [ arr[i+1], arr[i]];
-				operations.push({ index: [i, i+1], swap: true})
+				[ array[i], array[i+1]] = [ array[i+1], array[i]];
+				actions.push({ index: [i, i+1], swap: true})
 			}else{
-				operations.push({ index: [i, i+1], swap: false})
+				actions.push({ index: [i, i+1], swap: false})
 			}
 		}
 	}while(isSwapped);
-	return operations;
+	return actions;
 }
 
 
 // INSERTION SORT.
-function insertionSort(array){
-
-	let operations = [];
+function insertionSort(arg){
+	const array = [...arg];
+	let actions = [];
 	for ( let i=1; i<array.length; i++){
 		let key = array[i];
 		let j = i -1;
 
 		while ( j >=0 && array[j] > key ) { 
 			array[j +1] = array[j];
-			operations.push({index:[j+1, j] ,swap:true});
+			actions.push({index:[j+1, j] ,swap:true});
 			j = j -1;
 		}
 		array[j+1] = key;
-		operations.push({index:[j+1, j] ,swap:false});
+		actions.push({index:[j+1, j] ,swap:false});
 	}
-	return operations;
+	return actions;
 };
 
 
 // MERGE SORT.
-function mergeSort(array){
+function mergeSort(arg){
+	const array = [...arg];
+	let actions = [];
 
-	if(array.length === 1){ return array };
-	let middle = Math.floor(array.length/2);
-	let left = mergeSort(array.slice(0, middle));
-	let right = mergeSort(array.slice(middle));
+	//DIVIDE AND SORT
+	function divide(parameter){
+		if(parameter.length === 1){
+			return parameter
+		};
+		let middle = Math.floor(parameter.length/2);
+		let left = divide(parameter.slice(0, middle));
+		let right = divide(parameter.slice(middle));
+		let merged = merge(left, right);
 
-	return merge(left, right);
-};
+		return merged;
+	};
 
-function merge (array1, array2) {
-	console.log(array1, array2)
-	let merged = [];
-	let i = 0, j = 0;
-	while (i <array1.length && j <array2.length) {
-		if (array1[i] < array2[j]){
+	//MERGE ITEMS
+	function merge (array1, array2) {
+		let merged = [];
+		let i = 0, j = 0;
+
+		while (i <array1.length && j <array2.length) {
+			if (array1[i] < array2[j]){
+				merged.push(array1[i]);
+				// actions.push({index:[ obj[ array1[i] ] , obj[ array2[j] ] ] ,swap:false})//test
+				i++;
+			}else{
+				merged.push(array2[j]);
+				j++;
+			}
+		};
+		while (i <array1.length ){
 			merged.push(array1[i]);
 			i++;
-		}else{
-			merged.push(array2[j]);
+		};
+		while (j <array2.length){
+			merged.push( array2[j] );
 			j++;
-		}
+		};
+		return merged;
 	};
-	while (i <array1.length ){
-		merged.push(array1[i]);
-		i++;
-	};
-	while (j <array2.length){
-		merged.push( array2[j] );
-		j++;
-	};
-	return merged;
-	// return operations;
+	divide(array);
+	return actions;
+
 };
 
 
-// QUICK SORT
-function quickSort(){};
+// QUICK SORT.
+function quickSort(arg){};
 
+// HEAP SORT.
+function heapSort(arg){};
 
-// HEAP SORT
-function heapSort(){};
+// COUNTING SORT.
+function countingSort(arg){};
+
+// RADIX SORT
+function radixSort(arg){};
