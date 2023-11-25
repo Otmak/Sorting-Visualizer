566b77ad2564f301e10e83386292f967b41a34cf
otmak <jakelovz@gmail.com>
2023-11-25 14:06:30 -0800

    tab to spaces (*^_^)

--- column.js
+++ column.js
@@ -1,61 +1,61 @@
 // By Otuma.
 class Column{
-	constructor(x,y,width,height){
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-		this.queue = [];
-		this.color = {
-			r:159,
-			g:149,
-			b:173
-		}
-	}
-	// 153,141,169.deepPurple
-	//159,149,173.light
+    constructor(x,y,width,height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.queue = [];
+        this.color = {
+            r:159,
+            g:149,
+            b:173
+        }
+    }
+    // 153,141,169.deepPurple
+    //159,149,173.light
 
-	moveTo(location, framecount=5 ){
-		for(let i=0; i<=framecount; i++){
-			const t = i/framecount;
-			const u =  Math.sin(t*Math.PI);
-			this.queue.push({
-				x:linearInterpolate(this.x, location.x, t),
-				y:linearInterpolate(this.y, location.y, t),
-				r:linearInterpolate(159, 90, u),
-				g:linearInterpolate(149, 0, u),
-				b:linearInterpolate(173, 0, u)
-			})
-		}
-	}
+    moveTo(location, framecount=5 ){
+        for(let i=0; i<=framecount; i++){
+            const t = i/framecount;
+            const u =  Math.sin(t*Math.PI);
+            this.queue.push({
+                x:linearInterpolate(this.x, location.x, t),
+                y:linearInterpolate(this.y, location.y, t),
+                r:linearInterpolate(159, 90, u),
+                g:linearInterpolate(149, 0, u),
+                b:linearInterpolate(173, 0, u)
+            })
+        }
+    }
 
 
-	draw(ctx){
-		let processing = false;
-		if( this.queue.length > 0 ){
-			const {x,y,r,g,b} = this.queue.shift();
-			this.x = x;
-			this.y = y;
-			this.color = {r,g,b}
-			processing = true;
-		}
-		const left = this.x - this.width/2;
-		const top = this.y - this.height;
-		const right = this.x + this.width/2;
-		const {r,g,b} = this.color;
+    draw(ctx){
+        let processing = false;
+        if( this.queue.length > 0 ){
+            const {x,y,r,g,b} = this.queue.shift();
+            this.x = x;
+            this.y = y;
+            this.color = {r,g,b}
+            processing = true;
+        }
+        const left = this.x - this.width/2;
+        const top = this.y - this.height;
+        const right = this.x + this.width/2;
+        const {r,g,b} = this.color;
 
-		ctx.beginPath()
-		ctx.fillStyle = `rgb(${r},${g},${b})`;
+        ctx.beginPath()
+        ctx.fillStyle = `rgb(${r},${g},${b})`;
 
-		ctx.moveTo(left, top);
-		ctx.lineTo(left, this.y);
-		ctx.ellipse(this.x, this.y, this.width/2, this.width/4, 0, Math.PI, Math.PI*2, true);
+        ctx.moveTo(left, top);
+        ctx.lineTo(left, this.y);
+        ctx.ellipse(this.x, this.y, this.width/2, this.width/4, 0, Math.PI, Math.PI*2, true);
 
-		ctx.lineTo(right,top);
-		ctx.ellipse(this.x, top, this.width/2, this.width/4, 0, 0, Math.PI*2, true);
+        ctx.lineTo(right,top);
+        ctx.ellipse(this.x, top, this.width/2, this.width/4, 0, 0, Math.PI*2, true);
 
-		ctx.fill();
-		ctx.stroke();
-		return processing;
-	}
+        ctx.fill();
+        ctx.stroke();
+        return processing;
+    }
 }
--- main.js
+++ main.js
@@ -24,114 +24,116 @@ let framesPerSecond;
 // Create array of random numbers between 0 and 1.
 initiateArray();
 function initiateArray(){
-	for(let i=0; i<numberOfColums; i++){
-		const randomNum =  Math.random()
-		arr[i] = randomNum;
-		obj[randomNum] = i;
-	}
-	paint();
-	requestAnimationFrame(animate);
+    for(let i=0; i<numberOfColums; i++){
+        const randomNum =  Math.random()
+        arr[i] = randomNum;
+        obj[randomNum] = i;
+    }
+    paint();
+    requestAnimationFrame(animate);
 };
 
 
 // Adjust the number of Columns.
 function addColumnsToArray(){
-	let text = document.getElementById('rangeValue');
-	let newNumberOfColums = document.getElementById('rangeNumber').value;
-	text.innerHTML = newNumberOfColums;
-	numberOfColums = Number(newNumberOfColums);
-	cols = [];
-	arr = [];
-	obj = [];
-	spacing = (mainCanvas.width - margin*2)/numberOfColums;
-	initiateArray();
+    let text = document.getElementById('rangeValue');
+    let newNumberOfColums = document.getElementById('rangeNumber').value;
+    text.innerHTML = newNumberOfColums;
+    numberOfColums = Number(newNumberOfColums);
+    cols = [];
+    arr = [];
+    obj = [];
+    spacing = (mainCanvas.width - margin*2)/numberOfColums;
+    initiateArray();
 }
 
 
 // Generate Columns.
 function paint(){
-	operations = [];
-	for(let i=0; i<arr.length; i++){
-		const x = i*spacing+spacing/2 + margin;
-		const y = mainCanvas.height - margin;
-		const width = spacing -4;
-		const height = (mainCanvas.height - margin*2)*arr[i];
-		cols[i] = new Column(x,y,width,height);
-	}
+    operations = [];
+    for(let i=0; i<arr.length; i++){
+        const x = i*spacing+spacing/2 + margin;
+        const y = mainCanvas.height - margin;
+        const width = spacing -4;
+        const height = (mainCanvas.height - margin*2)*arr[i];
+        cols[i] = new Column(x,y,width,height);
+    }
 }
 
 
 //Algorithm selection
 const selectAgorithm = (arg, i) =>{
-	const array = [...arg];
-	const object = {
-		a: bubbleSort(array),
-		b: insertionSort(array),
-		c: mergeSort(array),
-		d: quickSort(array),
-		e: heapSort(array)
-	}
-	return object[i];
+    const array = [...arg];
+    const object = {
+        a: bubbleSort(array),
+        b: insertionSort(array),
+        c: mergeSort(array),
+        d: quickSort(array),
+        e: heapSort(array)
+    }
+    console.log('RUNNING OBJECT',object[i])
+    return object[i];
+
 }
 
 
 // Algorithm selection event listener.
 selectedBtn.addEventListener('click', (e)=>{
-	if(e.target.nodeName === 'SPAN'){
-		paint();
-		chosenAlgorithm = e.target.id;
-		isOkayToRun = true;
-	}
+    if(e.target.nodeName === 'SPAN'){
+        paint();
+        chosenAlgorithm = e.target.id;
+        isOkayToRun = true;
+    }
 });
 
 
 // Change the animation speed.
 framesPerSecondSelected.addEventListener('change', (e)=>{
-	framesPerSecond = Number(e.srcElement.value);
+    framesPerSecond = Number(e.srcElement.value);
 })
 
 
 // Run the Algorithm.
 function runAlgorithm(){
-	// operations = []; // to pause or stop the animation when it's running
-	if(isOkayToRun){
-		paint();
-		operations = selectAgorithm(arr, chosenAlgorithm);
-		requestAnimationFrame(animate);
-	}
+    // operations = []; // to pause or stop the animation when it's running
+    if(isOkayToRun){
+        paint();
+        operations = selectAgorithm(arr, chosenAlgorithm);
+        requestAnimationFrame(animate);
+    }
 }
 
 
 //Linear interpolation.
 function linearInterpolate(a, b, t){
-	return a + (b - a) * t;
+    return a + (b - a) * t;
 };
 
 
 function animate(){
-	let counter = 0;
-	ctx.clearRect(0,0, mainCanvas.width, mainCanvas.height);
-	let processing = false;
-	for( let i=0; i<cols.length; i++ ){
-		processing = cols[i].draw(ctx) || processing;
-		counter++
-	}
-	// if undifined or something give an alert;
-	if( !processing && operations.length >0 ){
-		const action = operations.shift();
-		const [i,j] = action.index;
-		if( action.swap ){
-			framesPerSecond ? cols[i].moveTo( cols[j], framesPerSecond) : cols[i].moveTo( cols[j] );
-			framesPerSecond ? cols[j].moveTo( cols[i], framesPerSecond) : cols[j].moveTo( cols[i] );
-			[ cols[i], cols[j] ] = [ cols[j], cols[i] ]; //Swap
-		}
-	}
-	if(operations.length > 0){
-		isOkayToRun = false;
-		requestAnimationFrame(animate);
-	}else{
-		isOkayToRun = true;
-	}
+    let counter = 0;
+    ctx.clearRect(0,0, mainCanvas.width, mainCanvas.height);
+    let processing = false;
+    for( let i=0; i<cols.length; i++ ){
+        processing = cols[i].draw(ctx) || processing;
+        counter++
+    }
+    // if undifined or something give an alert;
+    if( !processing && operations.length >0 ){
+        const action = operations.shift();
+        const [i,j] = action.index;
+        if( action.swap ){
+            framesPerSecond ? cols[i].moveTo( cols[j], framesPerSecond) : cols[i].moveTo( cols[j] );
+            framesPerSecond ? cols[j].moveTo( cols[i], framesPerSecond) : cols[j].moveTo( cols[i] );
+            [ cols[i], cols[j] ] = [ cols[j], cols[i] ]; //Swap
+        }
+    }
+    if(operations.length > 0){
+        isOkayToRun = false;
+        requestAnimationFrame(animate);
+    }else{
+        isOkayToRun = true;
+    }
 };
 
 
@@ -140,166 +142,177 @@ function animate(){
 
 // BUBBLE SORT.
 function bubbleSort(arg){
-	const array = [...arg];
-	const actions = [];
-	// let isSwapped = false;
-	do{
-		var isSwapped = false;
-		for(let i=0; i<array.length; i++){
-			let current = array[i];
-			if(array[i] > array[i+1]){
-				isSwapped = true;
-				[ array[i], array[i+1]] = [ array[i+1], array[i]];
-				actions.push({ index: [i, i+1], swap: true})
-			}else{
-				actions.push({ index: [i, i+1], swap: false})
-			}
-		}
-	}while(isSwapped);
-	return actions;
+    const array = [...arg];
+    const actions = [];
+    // let isSwapped = false;
+    do{
+        var isSwapped = false;
+        for(let i=0; i<array.length; i++){
+            let current = array[i];
+            if(array[i] > array[i+1]){
+                isSwapped = true;
+                [ array[i], array[i+1]] = [ array[i+1], array[i]];
+                actions.push({ index: [i, i+1], swap: true})
+            }else{
+                actions.push({ index: [i, i+1], swap: false})
+            }
+        }
+    }while(isSwapped);
+    return actions;
 }
 
 
 // INSERTION SORT.
 function insertionSort(arg){
-	const array = [...arg];
-	let actions = [];
-	for ( let i=1; i<array.length; i++){
-		let key = array[i];
-		let j = i -1;
+    const array = [...arg];
+    let actions = [];
+    for ( let i=1; i<array.length; i++){
+        let key = array[i];
+        let j = i -1;
 
-		while ( j >=0 && array[j] > key ) { 
-			array[j +1] = array[j];
-			actions.push({index:[j+1, j] ,swap:true});
-			j = j -1;
-		}
-		array[j+1] = key;
-		actions.push({index:[j+1, j] ,swap:false});
-	}
-	return actions;
+        while ( j >=0 && array[j] > key ) { 
+            array[j +1] = array[j];
+            actions.push({index:[j+1, j] ,swap:true});
+            j = j -1;
+        }
+        array[j+1] = key;
+        actions.push({index:[j+1, j] ,swap:false});
+    }
+    return actions;
 };
 
 
 // MERGE SORT.
 function mergeSort(arg){
-	const mainArray = [...arg];
-	let actions = [];
+    const mainArray = [...arg];
+    const test = [...arg];
+    let actions = [];
 
-	//DIVIDE AND SORT
-	function divide(array){
-		if(array.length === 1){
-			return array
-		};
-		let middle = Math.floor(array.length/2);
-		let left = divide(array.slice(0, middle));
-		let right = divide(array.slice(middle));
-		let merged = merge(left, right);
+    //DIVIDE AND SORT
+    function divide(array){
+        if(array.length === 1){
+            return array
+        };
+        let middle = Math.floor(array.length/2);
+        let left = divide(array.slice(0, middle));
+        let right = divide(array.slice(middle));
+        let merged = merge(left, right);
+        console.log('Merged ====>', merged)
 
-		return merged;
-	};
+        return merged;
+    };
 
-	//MERGE ITEMS
-	function merge (array1, array2) {
-		let merged = [];
-		let i = 0, j = 0;
-		while (i <array1.length && j <array2.length) {
-			if (array1[i] < array2[j]){
-				merged.push(array1[i]);
-				// actions.push({index:[ obj[ array1[i] ] , obj[ array2[j] ] ] ,swap:false})//test
-				i++;
-			}else{
-				merged.push(array2[j]);
-				j++;
-			}
-		};
-		while (i <array1.length ){
-			merged.push(array1[i]);
-			i++;
-		};
-		while (j <array2.length){
-			merged.push( array2[j] );
-			j++;
-		};
-		return merged;
-	};
-	divide(mainArray);
-	return actions;
+    //MERGE ITEMS
+    function merge (array1, array2) {
+        let merged = [];
+        let i = 0, j = 0;
+        console.log('Original array', test)
+        console.log('Comparing', array1[i], array2[j] )
+        while (i <array1.length && j <array2.length) {
+            if (array1[i] < array2[j]){
+                console.log('left is less', array1[i])
+                merged.push(array1[i]);
+                // actions.push({index:[ obj[ array1[i] ] , obj[ array2[j] ] ] ,swap:false})//test
+                actions.push({index:[ i, j ] ,swap:false})//test
+                i++;
+            }else{
+                console.log('Not less need Swap', array2[i])
+                // actions.push({index:[ obj[ array1[i] ] , obj[ array2[j] ] ] ,swap:true})// ..
+                actions.push({index:[ i, j ] ,swap:true})// ..
+                merged.push(array2[j]);
+                j++;
+            }
+        };
+        while (i <array1.length ){
+            console.log('leftover in array 1', array1[i])
+            merged.push(array1[i]);
+            i++;
+        };
+        while (j <array2.length){
+            console.log('leftover in array 2',array2[j])
+            merged.push( array2[j] );
+            j++;
+        };
+        return merged;
+    };
+    divide(mainArray);
+    return actions;
 
 };
 
 
 // QUICK SORT.
 function quickSort(arg){
-	const mainArray = [...arg];
-	let actions = [];
+    const mainArray = [...arg];
+    let actions = [];
 
-	function sort(array, left=0, right=array.length-1){
-		let pivotIndex = pivot(array, left, right);
-		if(left < right){
-			sort(array, left, pivotIndex-1);
-			sort(array, pivotIndex+1, right);
-		}
-		return array;
-	};
+    function sort(array, left=0, right=array.length-1){
+        let pivotIndex = pivot(array, left, right);
+        if(left < right){
+            sort(array, left, pivotIndex-1);
+            sort(array, pivotIndex+1, right);
+        }
+        return array;
+    };
 
-	function pivot(array, pivotIndex=0, endIndex=array.length-1){
-	  let swapIndex = pivotIndex;
-	  for (let i = pivotIndex +1; i<= endIndex; i++) {
-	    if(array[i] < array[pivotIndex]) {
-	      swapIndex++;
-	      [array[i], array[swapIndex]] = [array[swapIndex], array[i]];
-	      actions.push({index:[i, swapIndex] ,swap:true});
-	    }else{
-	    	actions.push({index:[i, swapIndex] ,swap:false});
-	    }
-	  }
-	  [array[pivotIndex], array[swapIndex]] = [array[swapIndex], array[pivotIndex]];
-	  actions.push({index:[pivotIndex, swapIndex] ,swap:true});
-	  return swapIndex;
-	};
-	sort(mainArray);
-	return actions;
+    function pivot(array, pivotIndex=0, endIndex=array.length-1){
+      let swapIndex = pivotIndex;
+      for (let i = pivotIndex +1; i<= endIndex; i++) {
+        if(array[i] < array[pivotIndex]) {
+          swapIndex++;
+          [array[i], array[swapIndex]] = [array[swapIndex], array[i]];
+          actions.push({index:[i, swapIndex] ,swap:true});
+        }else{
+            actions.push({index:[i, swapIndex] ,swap:false});
+        }
+      }
+      [array[pivotIndex], array[swapIndex]] = [array[swapIndex], array[pivotIndex]];
+      actions.push({index:[pivotIndex, swapIndex] ,swap:true});
+      return swapIndex;
+    };
+    sort(mainArray);
+    return actions;
 };
 
 
 // HEAP SORT.
 function heapSort(arg){
-	const mainArray = [...arg];
-	let actions = [];
-	function sort(array){
-		let heapSize = array.length;
-		// Build heap
-		for(let i = Math.floor(heapSize/2) -1; i>=0; i--){
-			maxHeap(array, heapSize, i);
-		};
-	  // Swap max element with last element
-		for(let i = heapSize -1; i >0; i--){
-			[array[0], array[i]] = [array[i], array[0]];
-			actions.push({index:[0, i], swap:true})
-			maxHeap(array, i, 0);
-		};
-	};
+    const mainArray = [...arg];
+    let actions = [];
+    function sort(array){
+        let heapSize = array.length;
+        // Build heap
+        for(let i = Math.floor(heapSize/2) -1; i>=0; i--){
+            maxHeap(array, heapSize, i);
+        };
+      // Swap max element with last element
+        for(let i = heapSize -1; i >0; i--){
+            [array[0], array[i]] = [array[i], array[0]];
+            actions.push({index:[0, i], swap:true})
+            maxHeap(array, i, 0);
+        };
+    };
 
-	function maxHeap(array, heapSize, i){
-		let left = 2*i +1;
-		let right = 2*i +2;
-		let maxElement = i;
-		if(left < heapSize && array[left] > array[maxElement]){
-			maxElement = left;
-		}
-		if(right < heapSize && array[right] > array[maxElement]){
-			maxElement = right;
-		}
-		if(maxElement != i){
-			[array[i], array[maxElement]] = [array[maxElement], array[i]];
-			actions.push({index: [i, maxElement], swap:true});
-			maxHeap(array, heapSize, maxElement);
-		}else{
-			actions.push({index:[i, maxElement], swap:false})
-		}
-	}
-	sort(mainArray);
-	return actions;
+    function maxHeap(array, heapSize, i){
+        let left = 2*i +1;
+        let right = 2*i +2;
+        let maxElement = i;
+        if(left < heapSize && array[left] > array[maxElement]){
+            maxElement = left;
+        }
+        if(right < heapSize && array[right] > array[maxElement]){
+            maxElement = right;
+        }
+        if(maxElement != i){
+            [array[i], array[maxElement]] = [array[maxElement], array[i]];
+            actions.push({index: [i, maxElement], swap:true});
+            maxHeap(array, heapSize, maxElement);
+        }else{
+            actions.push({index:[i, maxElement], swap:false})
+        }
+    }
+    sort(mainArray);
+    return actions;
 
 };
 
